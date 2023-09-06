b9558a2d11c8bb92dcedc406f176960c52be26f9
Shelby Benton <shelby.benton@cru.org>
2023-09-05 18:11:37 -0600

    Updates

---
+++ precache-manifest.2f4afe8ac88caa0190e0db318c0d58a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5f6715d7827895bd27e",
+    "url": "https://dbenton9.github.io/cow-game/static/css/main.4f28020f.chunk.css"
+  },
+  {
+    "revision": "a5f6715d7827895bd27e",
+    "url": "https://dbenton9.github.io/cow-game/static/js/main.a5f6715d.chunk.js"
+  },
+  {
+    "revision": "31bed8541b011ec7d9ec",
+    "url": "https://dbenton9.github.io/cow-game/static/js/1.31bed854.chunk.js"
+  },
+  {
+    "revision": "3d88b252bd2720359f88",
+    "url": "https://dbenton9.github.io/cow-game/static/js/runtime~main.3d88b252.js"
+  },
+  {
+    "revision": "5a9ce15ac02b3a508455cd5610d5dc49",
+    "url": "https://dbenton9.github.io/cow-game/index.html"
+  }
+];
