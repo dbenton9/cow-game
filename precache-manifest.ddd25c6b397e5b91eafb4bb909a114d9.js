59fd259f8cd451f479dae476409270c474e53514
Shelby Benton <shelby.benton@cru.org>
2023-09-11 07:45:00 -0600

    Updates

---
+++ precache-manifest.ddd25c6b397e5b91eafb4bb909a114d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ea44992e53b5f71aece",
+    "url": "/cow-game/static/css/main.79fbcfd9.chunk.css"
+  },
+  {
+    "revision": "8ea44992e53b5f71aece",
+    "url": "/cow-game/static/js/main.8ea44992.chunk.js"
+  },
+  {
+    "revision": "29130e67553d7365f3b4",
+    "url": "/cow-game/static/css/1.14832127.chunk.css"
+  },
+  {
+    "revision": "29130e67553d7365f3b4",
+    "url": "/cow-game/static/js/1.29130e67.chunk.js"
+  },
+  {
+    "revision": "39812776db5be0d89a2e",
+    "url": "/cow-game/static/js/runtime~main.39812776.js"
+  },
+  {
+    "revision": "c31deb43949f44ff665eee5a69b14c24",
+    "url": "/cow-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cow-game/precache-manifest.0f176c8f713c36f28278730f22cd5921.js"
+  "/cow-game/precache-manifest.ddd25c6b397e5b91eafb4bb909a114d9.js"
 );
 
 workbox.clientsClaim();
